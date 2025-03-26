bdb0f6fc5dcdd8f2473dd870a285084a8a23b7a3
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-03-26 15:45:00 +0530

    done

--- src/app.js
+++ src/app.js
@@ -14,5 +14,17 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+
+// routes import 
+
+import userRouter from "./routes/user.routes.js "
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:5000/api/v1/users/register
+
 export { app };
  
