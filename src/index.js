631ef2de3e229d59266a64a9c4bedd773535689b
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-03-29 12:26:10 +0530

    bug fixed

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
 .then(() => {
   app.listen(process.env.PORT || 7000, () => {
-    console.log(`Server is running at port ${process.env.PORT}`);
+    console.log(`Server is running at port ${process.env.PORT || 7000}`);
     
   })
 })
