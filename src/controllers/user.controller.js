d864359d4ab4b49059488f8943f11aa244a6f6f7
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-04-03 16:03:49 +0530

    made some changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.model.js";
+import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -43,7 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Avatar file is required")
   }
 
-  const User = await User.create({
+  const user = await User.create({
     fullName,
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
@@ -52,7 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase()
   })
 
-  const createdUser = await User.findByid(username._id).select(
+  const createdUser = await User.findById(user._id).select(
     "-password -refershToken"
   )
   
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose, { Schema } from "mongoose";
-import { JsonWebTokenError } from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import pkg from "jsonwebtoken";
+
+const { JsonWebTokenError } = pkg;
 const userSchema = new Schema(
   {
     username: {
@@ -56,36 +58,34 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function
-(password){
-  return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
       email: this.email,
       username: this.username,
-      fullname: this.fullName
+      fullname: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn:process.envACCESS_TOKEN_EXPIRY
-    }
-  )
-}
-userSchema.methods.generateRefreshToken = function(){
+      expiresIn: process.envACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-     
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-    }
-  )
-}
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
- 
